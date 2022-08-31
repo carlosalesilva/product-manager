0b44c84289de4269254dc6ff9e5f66bc09c40562
carlosalesilva <Carlinhos@2509>
2022-08-31 17:31:00 -0300

    Additional project settings.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,72 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import Table from "./components/Table";
 
-function App() {
+
+function App(props) {
+  const [items, setItems] = useState([
+    {
+      id: 0,
+      first: "Jane",
+      last: "Smith",
+      email: "js@gmail.com",
+      phone: "535-555-4555",
+      location: "New York, New York",
+      hobby: "Programming"
+    }
+  ]);
+
+  const getItems = () => {
+    fetch("http://localhost:3000/crud")
+      .then((response) => response.json())
+      .then((items) => setItems(items))
+      .catch((err) => console.log(err));
+  };
+
+  const addItemToState = (item) => {
+    setItems([...items, item]);
+  };
+
+  const updateState = (item) => {
+    const itemIndex = items.findIndex((data) => data.id === item.id);
+    const newArray = [
+      ...items.slice(0, itemIndex),
+      item,
+      ...items.slice(itemIndex + 1)
+    ];
+    setItems(newArray);
+  };
+
+  const deleteItemFromState = (id) => {
+    const updatedItems = items.filter((item) => item.id !== id);
+    setItems(updatedItems);
+  };
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
   return (
-    <div className="App">
+    <Container className="App">
+      <Row>
+        <Col>
+          <h1 style={{ margin: "20px 0" }}>CRUD Database</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Table
+            items={items}
+            updateState={updateState}
+            deleteItemFromState={deleteItemFromState}
+          />
+        </Col>
+      </Row>
       
-    </div>
+    </Container>
   );
 }
 
 export default App;
+
